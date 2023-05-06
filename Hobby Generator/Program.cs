edb4e239658a87e95523481e297d426a38741dc5
Philip Olafsen <ph.olafsen@gmail.com>
2023-05-06 12:29:06 +0200

    BREAKING_CHANGE: added hobbygenerator program. Not sure if its all the needed files.

--- Hobby Generator/Program.cs
+++ Hobby Generator/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Hobby_Generator
+﻿
+namespace Hobby_Generator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] hobbies = {"Fishing","Dancing", "Singing", "Playing video-games"};
+            Console.WriteLine("Enter name to receive new hobby! : ");
+            var person = Console.ReadLine() ?? throw new ArgumentNullException("Console.ReadLine()");
+            Console.WriteLine($"Your name is {person}");
+
+                var rand = new Random();
+                int randomNumber = rand.Next(hobbies.Length);
+                Console.WriteLine($"Dette er randomNumber ->{randomNumber}");
+                Console.WriteLine($"{person} has picked up a new hobby! {hobbies[randomNumber]}");
         }
     }
 }
