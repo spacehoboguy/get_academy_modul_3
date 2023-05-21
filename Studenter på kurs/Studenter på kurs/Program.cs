2476426939c7baf7e55a7faf08ee1b46e03e49b0
Philip Olafsen <ph.olafsen@gmail.com>
2023-05-21 15:40:02 +0200

    added working code to ukesoppg 2

---
+++ Studenter på kurs/Studenter på kurs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Studenter_på_kurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
